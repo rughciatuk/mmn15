9143977e812b7c2a9d2dd6caa5948d5a587c040f
Roi Ben - Shaul <roi19997@gmail.com>
2022-09-02 19:22:20 +0300

    Reading port from the file

--- server/main.py
+++ server/main.py
@@ -1,4 +1,15 @@
+PORT_FILE_NAME = "port.info"
+
+
+def read_port():
+    try:
+        with open("port.info", "r") as port_file:
+            return int(port_file.readline())
+    except FileNotFoundError:
+        print("Warning: port.info doesn't exists, using 1234 as the port")
+        return 1234
 
 
 if __name__ == '__main__':
     print("This is the server")
+    print(read_port())
